3e5a2fed06e0ca5222a53f77572ca25d84932606
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-11-15 21:43:06 +0900

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5fa9a7e975b098c9916f039a79d1c6744e1bc0da',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa9a7e975b098c9916f039a79d1c6744e1bc0da',
+    ),
+  ),
+);
